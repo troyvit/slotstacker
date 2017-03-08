334499ba23f52b8b2027a359bd37f307763869aa
Troy Vitullo <github@troyvit.com>
2017-03-07 23:03:49 -0700

    remove about, clean up debugging

--- human_replacement.php
+++ human_replacement.php
@@ -27,7 +27,7 @@ class robot {
             }
             echo "$slot: $block_stack\n";
         }
-        echo "\n\n";
+        echo "";
     }
 
 	function make_slots($size) {
@@ -140,19 +140,9 @@ class robot {
         // I wrote a command interpreter for a fairly complex API that leveraged the API's grammar to execute its commands. It was a lot of backend work that took many hours, so for this I'll keep it simple (and not as extensible).
 
         if($command_name == 'size') {
-            echo "we are in size!!!\n\n\n\n\n";
             // not much error checking here. We know it's a number by now
             $size = $command_arr[0];
             $slot_count = count($slots);
-            if($size < $slot_count) {
-                // we are losing slots. Time to bail and reset the whole thing
-                /*
-                $this -> slots='';
-                $this -> slot_state = array();
-                echo "truncating command_list because fear \n";
-                $this -> command_list = array();
-                 */
-            }
             $this -> make_slots($size);
         }
 
@@ -227,73 +217,6 @@ class robot {
         }
 	}
 }
-
-$about =
-"The Human Replacement Script aims to replace all humans who wish to place boxes into slots. It is superior in every way to modern human brains. Howto and features:
-
-+ Create a new set of slots by typing:
-
-size [n]
-
-where [n] is the number of slots you want.
-
-+ Add a box to a slot by typing:
-
-add [n]
-
-where [n] is the number of the slot you would like to add the box to.
-
-+ Remove a box from a slot by typing:
-
-rm [n]
-
-where [n] is the number of the slot from which you would like to remove the box.
-
-+ Move a box from one slot to another by typing:
-
-mv [n] [x]
-
-where [n] is the slot from which you move a box and [x] is the destination.
-
-+ Undo movements by typing:
-
-undo [n]
-
-where [n] is how far back in history you would like to go with an undo. For instance if you type:
-
-add 1
-add 2
-add 3
-undo 2
-
-the robot will remove one box from slot 2 and one box from slot 3.
-
-+ replay a past movement by typing:
-
-replay [n]
-
-where [n] is how far back in history you would like to go to make a replay. 
-
-For instance if you type:
-
-add 1
-add 2
-add 3
-replay 2
-
-the robot will add a new box to slot 2 and a new box to slot 3.
-
-NOTE: undo and replay are not stored in history, so you cannot undo an undo, or replay a replay.
-
-+ Exit the program by typing:
-
-exit
-
-Start by creating some slots for the boxes by typing:
-                
-size [n]
-
-where [n] is the number of slots you would like\n\n> ";
 
 $r = new robot;
 $command = '';
